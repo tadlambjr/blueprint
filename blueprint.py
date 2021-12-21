193eea9cf5e3d76b1c9e3e5d2066b0b6592c5ee2
cah-tad-lamb <tad.lamb@cardinalhealth.com>
2021-12-21 18:09:10 -0500

    initial version

---
+++ blueprint.py
@@ -0,0 +1,33 @@
+import os
+import configparser
+import pathlib
+import sys
+from controller import Controller
+
+rootdir = '/Users/tad.lamb/devl/ofl-next'
+
+java_file = '/Users/tad.lamb/devl/ofl-next/accounting/ofl-next-accounting-experience-api/src/main/java/com/cardinalhealth/accounting/experience/controller/PendingReviewExperienceController.java'
+
+controllers = []
+
+def load_properties():
+    config = configparser.ConfigParser()
+    config.read('blueprint.properties')
+    project_root = config.get('General', 'project-root')
+    print(f'Root directory: {project_root}\n')
+
+def get_mappings(filename):
+    controller = Controller(filename)
+    controller.parse_file()
+    controllers.append(controller)
+
+if __name__ == "__main__":
+    load_properties()
+    get_mappings(java_file)
+    for controller in controllers:
+        print(controller)
+    # for subdir, dirs, files in os.walk('.'):
+    #     for file in files:
+    #         print(file)
+    #         print os.path.join(subdir, file)
+
---
+++ controller.py
@@ -0,0 +1,56 @@
+import re
+
+class Controller:
+    """A Controller class"""
+
+    authentication_re = re.compile(r'^\@PreAuthorize\(\"isAuthenticated\(\)\"\)')
+    authentication_missing = True
+
+    request_matching_re = re.compile(r'^\@RequestMapping\(\"(.*)\"\)')
+    mapping_root = ''
+
+    get_matching_re    = re.compile(r'\s+\@GetMapping\(\"(.*)\"\)')
+    post_matching_re   = re.compile(r'\s+\@PostMapping\(\"(.*)\"\)')
+    put_matching_re    = re.compile(r'\s+\@PutMapping\(\"(.*)\"\)')
+    patch_matching_re  = re.compile(r'\s+\@PatchMapping\(\"(.*)\"\)')
+    delete_matching_re = re.compile(r'\s+\@DeleteMapping\(\"(.*)\"\)')
+    mappings = []
+
+    def __init__(self, filename):
+        self._filename = filename
+        self._class_name = re.match(r'^.*\/(.*)\.java$', filename).group(1)
+
+    def parse_file(self):
+        lines = open(self._filename).readlines()
+        for line in lines:
+            match = self.authentication_re.match(line)
+            if match:
+                self.authentication_missing = False
+            match = self.request_matching_re.match(line)
+            if match:
+                self.mapping_root = match.group(1)
+            match = self.get_matching_re.match(line)
+            if match:
+                self.mappings.append(f'GET {self.mapping_root}{match.group(1)}')
+            match = self.post_matching_re.match(line)
+            if match:
+                self.mappings.append(f'POST {self.mapping_root}{match.group(1)}')
+            match = self.put_matching_re.match(line)
+            if match:
+                self.mappings.append(f'PUT {self.mapping_root}{match.group(1)}')
+            match = self.patch_matching_re.match(line)
+            if match:
+                self.mappings.append(f'PATCH {self.mapping_root}{match.group(1)}')
+            match = self.delete_matching_re.match(line)
+            if match:
+                self.mappings.append(f'DELETE {self.mapping_root}{match.group(1)}')
+
+    def get_filename(self):
+        return self._filename
+
+    def __str__(self):
+        mappings = "\n\t".join(self.mappings)
+        secured = '  ' if self.authentication_missing else u'\U0001F512 '
+        return f'{secured}{self._class_name}\n\t{mappings}'
+
+# @PreAuthorize("isAuthenticated()")
