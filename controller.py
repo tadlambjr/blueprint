2591ba1e9f628f07d9ed23239d4c0526a429b37e
cah-tad-lamb <tad.lamb@cardinalhealth.com>
2021-12-23 15:41:57 -0500

    cleaned up matching

--- controller.py
+++ controller.py
@@ -9,12 +9,18 @@ class Controller:
 
     request_matching_re = re.compile(r'^\@RequestMapping\(\"(.*)\"\)')
 
-    get_matching_def_re = re.compile(r'\s+\@GetMapping\(\"(.*)\"\)')
-    get_matching_re     = re.compile(r'\s+\@GetMapping\(\"(.*)\"\)')
-    post_matching_re    = re.compile(r'\s+\@PostMapping\(\"(.*)\"\)')
-    put_matching_re     = re.compile(r'\s+\@PutMapping\(\"(.*)\"\)')
-    patch_matching_re   = re.compile(r'\s+\@PatchMapping\(\"(.*)\"\)')
-    delete_matching_re  = re.compile(r'\s+\@DeleteMapping\(\"(.*)\"\)')
+    get_matching_def_re   = re.compile(r'\s+\@GetMapping\(\"(.*)\"\)')
+
+    get_matching_val_re   = re.compile(r'\s+\@GetMapping\(value\s*=\s*\"(.*?)\".*\)')
+    get_matching_path_re  = re.compile(r'\s+\@GetMapping\(path\s*=\s*\"(.*?)\".*\)')
+    get_matching_re       = re.compile(r'\s+\@GetMapping\(\"(.*)\"\)')
+    post_matching_re      = re.compile(r'\s+\@PostMapping\(\"(.*)\"\)')
+    post_matching_val_re  = re.compile(r'\s+\@PostMapping\(value\s*=\s*\"(.*?)\".*\)')
+    post_matching_path_re = re.compile(r'\s+\@PostMapping\(path\s*=\s*\"(.*?)\".*\)')
+    put_matching_re       = re.compile(r'\s+\@PutMapping\(\"(.*)\"\)')
+    patch_matching_re     = re.compile(r'\s+\@PatchMapping\(\"(.*)\"\)')
+    delete_matching_re    = re.compile(r'\s+\@DeleteMapping\(\"(.*)\"\)')
+    delete_matching_path_re = re.compile(r'\s+\@DeleteMapping\(path\s*=\s*\"(.*?)\"\)')
 
     def __init__(self, filename):
         self._filename = filename
@@ -23,32 +29,37 @@ class Controller:
         self.mappings = []
         self.parse_file()
         logging.info(self)
+    
+    def add_match(self, line, regex, rest_type):
+        match = regex.match(line)
+        if match:
+            self.mappings.append(f'{rest_type} {self.mapping_root}{match.group(1)}')            
 
     def parse_file(self):
         in_class = False
         lines = open(self._filename).readlines()
         for line in lines:
+            if line.startswith('public class'):
+                in_class = True
+
             match = self.authentication_re.match(line)
             if match:
                 self.authentication_missing = False
+
             match = self.request_matching_re.match(line)
             if match:
                 self.mapping_root = match.group(1)
-            match = self.get_matching_re.match(line)
-            if match:
-                self.mappings.append(f'GET {self.mapping_root}{match.group(1)}')
-            match = self.post_matching_re.match(line)
-            if match:
-                self.mappings.append(f'POST {self.mapping_root}{match.group(1)}')
-            match = self.put_matching_re.match(line)
-            if match:
-                self.mappings.append(f'PUT {self.mapping_root}{match.group(1)}')
-            match = self.patch_matching_re.match(line)
-            if match:
-                self.mappings.append(f'PATCH {self.mapping_root}{match.group(1)}')
-            match = self.delete_matching_re.match(line)
-            if match:
-                self.mappings.append(f'DELETE {self.mapping_root}{match.group(1)}')
+
+            self.add_match(line, self.get_matching_re, 'GET')
+            self.add_match(line, self.get_matching_val_re, 'GET')
+            self.add_match(line, self.get_matching_path_re, 'GET')
+            self.add_match(line, self.post_matching_re, 'POST')
+            self.add_match(line, self.post_matching_val_re, 'POST')
+            self.add_match(line, self.post_matching_path_re, 'POST')
+            self.add_match(line, self.put_matching_re, 'PUT')
+            self.add_match(line, self.patch_matching_re, 'PATCH')
+            self.add_match(line, self.delete_matching_re, 'DELETE')
+            self.add_match(line, self.delete_matching_path_re, 'DELETE')
 
     def get_filename(self):
         return self._filename
